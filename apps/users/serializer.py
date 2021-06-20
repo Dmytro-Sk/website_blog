20c38197a230d9a728d0a541c46c0d281049fc20
Dmytro-Sk <0971898920d@gmail.com>
2021-06-20 15:47:57 +0300

    Change user name for representation in rest api

--- apps/users/serializer.py
+++ apps/users/serializer.py
@@ -6,5 +6,11 @@ from .models import Profile
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
-        fields = '__all__'
-        # fields = ['user', 'user_image']
+        # fields = '__all__'
+        fields = ['user', 'user_image', 'user_image', 'website_url', 'facebook_url', 'twitter_url', 'instagram_url',
+                  'pinterest_url', ]
+
+    def to_representation(self, instance):
+        representation = super(ProfileSerializer, self).to_representation(instance)
+        representation['user'] = instance.user.first_name
+        return representation
