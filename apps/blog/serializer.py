de43f9752975302ede7e5f7c5d1b6baad2a054a9
Dmytro-Sk <0971898920d@gmail.com>
2021-06-20 12:51:28 +0300

    Change date format for representation in rest api

--- apps/blog/serializer.py
+++ apps/blog/serializer.py
@@ -8,3 +8,8 @@ class PostSerializer(serializers.ModelSerializer):
         model = Post
         # fields = '__all__'
         fields = ['title', 'author', 'body', 'add_post_date', 'category', 'likes', 'dislikes']
+
+    def to_representation(self, instance):
+        representation = super(PostSerializer, self).to_representation(instance)
+        representation['add_post_date'] = instance.add_post_date.strftime('%d-%m-%Y')
+        return representation
